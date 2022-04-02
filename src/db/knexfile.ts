70d6cb82060ccd620a68de0be4938ee3e898a1f8
Duc Khanh <duckhanh97pt@gmail.com>
2022-04-02 10:44:41 +0700

    Conect knex

---
+++ src/db/knexfile.ts
@@ -0,0 +1,41 @@
+// Update with your config settings.
+const knexConfig = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      port: 3306,
+      database: 'webshop',
+      user:     'root',
+      password: 'mauFJcuf5dhRMQrjj'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: 'migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+
+export default knexConfig;
