c9b0c96145842896995183f1ceec782399962691
Duc Khanh <duckhanh97pt@gmail.com>
2022-04-15 15:55:36 +0700

    a

--- src/db/migrations/20220415082609_brands.ts
+++ src/db/migrations/20220415082609_brands.ts
@@ -2,19 +2,20 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('users', table => {
-    table.increments('id').unique().primary();
+  return knex.schema.createTable('brands', table => {
+    table.increments('id').unique();
     table.string('first_name', 255).notNullable();
     table.string('last_name', 255).notNullable();
-    table.string('email', 255).notNullable().unique();
+    table.string('email', 255).notNullable();
     table.string('password', 255).notNullable();
-    table.timestamps(true, true);
+    table.timestamps();
   })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema
-      .dropTable("users");
+      .dropTable("brands");
 }
 
+// 
